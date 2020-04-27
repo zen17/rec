805ab7192f4d7335bb780ea7ef4f16be1e6aa860
unknown <nemanja.jocic.dev@gmail.com>
2020-04-26 21:25:09 -0700

    docker postgres bug fixed

--- fe/src/app/modules/core/services/root.service.ts
+++ fe/src/app/modules/core/services/root.service.ts
@@ -8,8 +8,8 @@ export class RootService {
   public readonly rootApiUrl: string;
 
   constructor() {
-    // this.rootApiUrl = environment.rootApiUrl;
-    this.rootApiUrl = 'https://rec-be-dot-recenzija-270220.appspot.com/api';
+    this.rootApiUrl = environment.rootApiUrl;
+   // this.rootApiUrl = 'https://rec-be-dot-recenzija-270220.appspot.com/api';
     console.log('ANGULAR ENV,', environment);
   }
 }
